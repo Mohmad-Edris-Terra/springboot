fa7e14c93e0af3054ac2a9fb1df6cd683548b934
Mohmad Edris <mohmad.edris@terralogic.com>
2020-09-24 17:13:58 +0530

    modified changes

--- src/main/java/com/samplespringproject/springbootproject/controller/MainController.java
+++ src/main/java/com/samplespringproject/springbootproject/controller/MainController.java
@@ -2,19 +2,23 @@ package com.samplespringproject.springbootproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
 	@RequestMapping("/")
+	@ResponseBody
 	public String home() {
 		System.out.println("this is home page");
-		return "home";
+		return "This is home page";
+//		return "home";
 	}
 	
 	@RequestMapping("/contact")
 	public String contact() {
 		System.out.println("this is contact page");
-		return "contact";
+		return "this is contact page";
+//		return "contact";
 	}
 }
